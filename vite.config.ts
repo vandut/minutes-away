092bad825b51e33262202135f7cdd5d07e451a04
vandut <konrad.bielak@gmail.com>
2025-06-27 00:55:55 +0200

    Configure app for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/minutes-away/',
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
